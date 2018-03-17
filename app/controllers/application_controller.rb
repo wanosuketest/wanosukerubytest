ea7ca509c20f378ae684c2a31b3f3a18efd6aa35
wanosuketest <wanosukerubytest@gmail.com>
2018-03-17 16:37:25 +0900

    t

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 	private
 		# ユーザーのログインを確認する
 		def logged_in_user
-			unless logged_in?
-			# store_location
-			flash[:danger] = "Please log in."
-			redirect_to login_url
+				unless logged_in?
+				# store_location
+				flash[:danger] = "Please log in."
+				redirect_to login_url
+			end
 		end
-	end
 end
