30da6afdbf688c6e5fa25f8a528843f8e579bf9b
wanosuketest <wanosukerubytest@gmail.com>
2018-03-17 18:32:13 +0900

    log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	include SessionsHelper
 
 	private
-		# ユーザーのログインを確認する
+		# ユーザーのログインを確認するssu
 		def logged_in_user
 			unless logged_in?
 				# store_location
